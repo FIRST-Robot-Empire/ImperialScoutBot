1274dc608772d2a25827533e9abe5337ba349718
GoldmanJ <joshuabgoldman@gmail.com>
2019-12-28 16:29:03 -0500

    Added basic structure for modular scouting form and required information

---
+++ ImperialScoutLibrary/ImperialScoutLibrary/Army.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImperialScoutLibrary
+{
+    [DataContract]
+    public class Army
+    {
+        #region Army Properties
+        [DataMember(Name = "factions")]
+        public Dictionary<Regiments, Faction> Factions { get; set; }
+        #endregion
+
+        #region Army Methods
+        public Army()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ ImperialScoutLibrary/ImperialScoutLibrary/GlobalTypes.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+
+[DataContract]
+public enum Regiments
+{
+    Blue1,
+    Blue2,
+    Blue3,
+    Red1,
+    Red2,
+    Red3
+};
+
+[DataContract]
+public enum Campaigns
+{
+    Practice,
+    Qualification,
+    Elimination,
+    RoundRobin
+};
+
+[DataContract]
+public enum Alliances
+{
+    Red,
+    Blue
+};
